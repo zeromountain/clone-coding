c57908aad10f7ff1f00c81fd78aa5594427e80c2
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-03-26 20:27:20 +0900

    firebase setup

---
+++ tweet/nwitter/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZvqhP_MEb6Kw129sTzLxUyiDnj1uU6Yc",
+  authDomain: "nwitter-388bd.firebaseapp.com",
+  projectId: "nwitter-388bd",
+  storageBucket: "nwitter-388bd.appspot.com",
+  messagingSenderId: "175913157160",
+  appId: "1:175913157160:web:3bbfdcb9a33a03802eeb1d"
+};
+
+export default firebase.initializeApp(firebaseConfig); 
