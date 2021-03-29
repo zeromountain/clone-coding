12132b45f89ba9d3f3095ee908793d3d144d6863
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-03-29 17:51:16 +0900

    create account

--- tweet/nwitter/src/routes/Auth.js
+++ tweet/nwitter/src/routes/Auth.js
@@ -1,8 +1,11 @@
+import { authService } from 'fbase';
 import React, {useState} from 'react';
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
   const onChange = (event) => {
     const {
       target: {name, value}
@@ -14,16 +17,29 @@ const Auth = () => {
     }
   }
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      let data;
+      if(newAccount) {
+        // create account
+        data = await authService.createUserWithEmailAndPassword(email, password);
+      } else {
+        // log in
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange}/>
+        <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
         <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-        <input type="submit" value="Log In" /> 
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} /> 
       </form>
       <div>
         <button>Continue with Google</button>
