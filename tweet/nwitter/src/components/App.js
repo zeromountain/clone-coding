6aff95d998bd1b0eb949a93a40d5661e2cc7297c
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-03-29 18:21:14 +0900

    LogIn

--- tweet/nwitter/src/components/App.js
+++ tweet/nwitter/src/components/App.js
@@ -1,13 +1,27 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import AppRouter from 'components/Router';
 import { authService } from 'fbase';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  // 컴포넌트가 마운트될때 실행
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, [])
+
 
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn}/>
+      {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing .... " }
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- tweet/nwitter/src/routes/Auth.js
+++ tweet/nwitter/src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
 
   const onChange = (event) => {
     const {
@@ -19,7 +20,7 @@ const Auth = () => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    try {
+    try {  
       let data;
       if(newAccount) {
         // create account
@@ -30,17 +31,21 @@ const Auth = () => {
       }
       console.log(data);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
     }
   }
+  
+  const toggleAccount = () => setNewAccount(prev => !prev);
 
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
         <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-        <input type="submit" value={newAccount ? "Create Account" : "Log In"} /> 
+        <input type="submit" value={newAccount ? "Create Account" : "Sign In"} /> 
+        {error}
       </form>
+      <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"} </span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
