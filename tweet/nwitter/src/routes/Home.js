69b92d0008263e41d9c4aff8f8b5d9c3eca6b8e1
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-03-30 19:39:46 +0900

    Home form and Database

--- tweet/nwitter/src/routes/Home.js
+++ tweet/nwitter/src/routes/Home.js
@@ -1,5 +1,24 @@
-import React from 'react';
- 
-const Home = () => <span>Home</span>;
+import React, {useState} from 'react';
+
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event; 
+    setNweet(value);
+  }
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="What's on your mind?" maxlength={120}/>
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
