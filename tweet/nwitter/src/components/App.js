cf7c0d081f32a4dde6a19dc37da9f2ae7c34c6bd
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-04-01 13:57:43 +0900

    realtime Nweets

--- tweet/nwitter/src/components/App.js
+++ tweet/nwitter/src/components/App.js
@@ -5,12 +5,14 @@ import { authService } from 'fbase';
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   
   // 컴포넌트가 마운트될때 실행
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if(user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -21,7 +23,7 @@ function App() {
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing .... " }
+      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/> : "Initializing .... " }
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- tweet/nwitter/src/routes/Home.js
+++ tweet/nwitter/src/routes/Home.js
@@ -1,30 +1,40 @@
 import { dbService } from 'fbase';
 import React, {useState, useEffect} from 'react';
 
-const Home = () => {
+const Home = ({ userObj }) => {
+  console.log(userObj);
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
-  const getNweets = async () => {
-    const dbNweets = await dbService.collection("nweets").get();
-    // console.log(dbNweets);
-    dbNweets.forEach((document) => {
-      const nweetObject = {
-        ...document.data(),
-        id: document.id,
-      }
-      // set function => give a function instead of value
-      setNweets(prev => [nweetObject, ...prev]);
-    });
-  }
+  // const getNweets = async () => {
+  //   const dbNweets = await dbService.collection("nweets").get();
+  //   // console.log(dbNweets);
+  //   dbNweets.forEach((document) => {
+  //     const nweetObject = {
+  //       ...document.data(),
+  //       id: document.id,
+  //     }
+  //     // set function => give a function instead of value
+  //     setNweets(prev => [nweetObject, ...prev]);
+  //   });
+  // };
   // after mounting component
   useEffect(() => {
-    getNweets();
+    // getNweets();
+    // onSnapsho =? 8~19 코드 비교
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      const nweetArray = snapshot.docs.map(doc => ({ 
+        id: doc.id, 
+        ...doc.data() 
+      }));
+      setNweets(nweetArray);
+    })
   }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("nweets").add({
-      nweet,
+      text: nweet,
       createdAt: Date.now(),
+      creatorId: userObj.uid, 
     });
     setNweet("");
   };
@@ -34,7 +44,7 @@ const Home = () => {
     } = event; 
     setNweet(value);
   }
-  console.log(nweets); 
+  // console.log(nweets); 
   return (
     <>
       <div>
@@ -47,7 +57,7 @@ const Home = () => {
         {nweets.map(nweet => {
           return(
             <div key={nweet.id}>
-              <h4>{nweet.nweet}</h4>
+              <h4>{nweet.text}</h4>
             </div>
           );
         })}
